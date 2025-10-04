a6aae8c6c25c5d6d2b6f246198c4cb14fa007359
Wai Hei CHAN <wai.chan-12@student.manchester.ac.uk>
2025-10-04 21:43:05 +0100

    Deleted neuron class, made more powerful layer class which is more optimised, since we are using numpy to do matrix multiplication rather than using loops

--- neuralLibrary.py
+++ neuralLibrary.py
@@ -3,27 +3,28 @@ import random
 
 
 def LeakyReLU(x, a=0.01): 
-    if x > 0: 
-        return x 
-    else: 
-        return a*x
+    return np.where(x > 0, x, a * x)
 
-class Neuron: 
-    def __init__(self, input_size, weights=None, bias=0, val=0): 
-        if weights == None: 
-            self.weights = np.array([random.uniform(-1,1) for i in range(input_size)])                  # weights must be same dimension as inputs 
+class Layer: 
+    def __init__(self, num_neurons, input_size, activation_func, override_weights=None, override_bias=None): 
+        self.activation_func = activation_func
+        if override_weights == None: 
+            self.weights = np.random.uniform(-1, 1, (input_size, num_neurons,))                              # weights[4, 0] means the weight going from 4th input to 0th output neuron
         else: 
-            self.weights = np.array(weights)
-        self.bias = bias                                                                                # one bias to go into activation func, since we need a1w1 + a2w2 + a3w3 + ... + b 
-        self.val = val 
-    def forward(self, func, inputs): 
-        activationValue = np.dot(inputs, self.weights) + self.bias                                      # a1w1 + a2w2 + a3w3 + ... + b 
-        self.val = func(activationValue)                                                                # allow picking custom activation function (e.g. Sigmoid, ReLu)
-        return self.val
-
+            self.weights = np.array(override_weights)
+        if override_bias == None: 
+            self.biases = np.zeros(num_neurons,)
+        else: 
+            self.biases = np.array(override_bias)
+        self.output = np.zeros(num_neurons,)
+    def forward(self, inputs): 
+        inputs = np.array(inputs)
+        z = np.dot(inputs, self.weights) + self.biases 
+        self.output = self.activation_func(z)
+        return self.output
 
 # testing code 
 
-outputNeuron = Neuron(input_size=2, weights=[2,2], bias=-3)
-outputNeuron.forward(LeakyReLU, [1,1])
-print(outputNeuron.val)
+ANDLayer = Layer(num_neurons=1, input_size=2, activation_func=LeakyReLU, override_weights=[[2],[2]], override_bias=[-3])
+ANDLayer.forward([1,1])
+print(ANDLayer.output)
